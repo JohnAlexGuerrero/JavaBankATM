7cdb710ea03361ebae3e9691324bce6698c20e9e
johnalexguerrero <johnalexguerrero@gmail.com>
2024-11-15 14:40:50 -0500

    add feature

---
+++ src/main/java/com/mycompany/javabankatm/JavaBankATM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabankatm;
+
+/**
+ *
+ * @author JOHN
+ */
+public class JavaBankATM {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to JavaBank ATM! Version control with Git.");
+    }
+}
